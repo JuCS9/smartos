2b019cf1f3fe2427dd23624e77cdb12f50e324f9
Julia <124396309+JuCS9@users.noreply.github.com>
2024-11-29 14:17:29 -0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,18 +1,30 @@
-const mysql = require('mysql2')
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+const mysql = require('mysql2');
 
+// Configuração do pool de conexões
 const pool = mysql.createPool({
-host: process.env.DB_HOST,
-user: process.env.u610580921_julia,
-password: process.env.Junior93720,
-database: process.env.u610580921_smartos,
-waitForConnections: true,
-connectionLimit: 10,
-queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-pool.getConnection((err, conn) => {
-if(err) console.log(err)
-console.log("Connected successfully")
-})
+// Teste de conexão
+(async () => {
+  try {
+    const connection = await pool.promise().getConnection();
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    connection.release(); // Libera a conexão
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error.message);
+  }
+})();
 
-module.exports = pool.promise()
+// Exporta o pool para uso em outros arquivos
+module.exports = pool.promise();
+
+
+

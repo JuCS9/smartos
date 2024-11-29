6caa0cc4e0798d8e4d4a7d492e4778885d277944
Julia <124396309+JuCS9@users.noreply.github.com>
2024-11-29 14:09:53 -0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
 host: process.env.DB_HOST,
-user: process.env.DB_USERNAME,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_DBNAME,
+user: process.env.u610580921_julia,
+password: process.env.Junior93720,
+database: process.env.u610580921_smartos,
 waitForConnections: true,
 connectionLimit: 10,
 queueLimit: 0
